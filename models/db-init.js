6bbfec887faab09741726293477c5bad46736d95
Seif Eldine <seif.ashraf@qeema.net>
2022-06-13 18:15:37 +0200

    update conn str

--- models/db-init.js
+++ models/db-init.js
@@ -3,8 +3,8 @@
 const { Sequelize } = require('sequelize');
 
 // Connect to DB
-module.exports = new Sequelize('sequelize_db', 'postgres', 'RoBeN44554488', {
-    host: 'localhost',
+module.exports = new Sequelize('d8nmqhi8r4mvmp', 'ofqwvoqzvyigqt', 'c0aba7d30f70c81ef21609509fb7fb21be842daa689fff99e4414bca099211d4', {
+    host: 'ec2-54-204-56-171.compute-1.amazonaws.com',
     dialect: 'postgres',
 });
 
